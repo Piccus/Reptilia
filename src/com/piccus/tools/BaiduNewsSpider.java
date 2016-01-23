35e530a3dfdc9a7c9fb03164a5403401cc92b7ec
Piccus <piccus@outlook.com>
2016-01-23 13:02:24 +0800

    basic complete baidunewsspider

--- src/com/piccus/tools/BaiduNewsSpider.java
+++ src/com/piccus/tools/BaiduNewsSpider.java
@@ -11,12 +11,45 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+
 public abstract class BaiduNewsSpider {
-	
+	//HaspMap存储标题和链接
 	private static HashMap<String, String> hm = new HashMap<String, String>();
+	//爬取关键字
+	private static String keyword = "";
+	//实际爬取链接
+	private static String realUrl = "http://news.baidu.com/ns?";
+	//默认时间顺序爬取
+	private static final int sortByTime = 0;
+	//默认爬取数量	
+	private static int listMax = 20;
+	//爬虫UserAgent
+	private static final String agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.231.154 Safari/537.36 LBBROWSER";
 	
+	/*
+	 * @param keyword
+	 * @param filename
+	 * @Author: Piccus
+	 * @Description: 初始化爬虫并开始爬取 
+	 */
+	public static void initSpider(String key, String filename){
+		keyword = key;
+		realUrl = realUrl + "word=" + keyword + "&tn=news&from=news&rn=" + listMax + "ct=" + sortByTime;
+		try {
+			showList(realUrl);
+			saveList(filename);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//System.out.println("success get " + listMax + " websites and saving to " + filename);
+	}
 	
-	public static void saveList(String filename) throws IOException{
+	/*
+	 * @param filename
+	 * @Author: Piccus
+	 * @Description: 保存HashMap到文件
+	 */
+	private static void saveList(String filename) throws IOException{
 
 		FileWriter fw = new FileWriter(filename);
 		BufferedWriter bw = new BufferedWriter(fw);
@@ -26,26 +59,43 @@ public abstract class BaiduNewsSpider {
 			bw.newLine();
 		}
 		bw.flush();
-		bw.close();
 		fw.close();
+		bw.close();
 	}
 	
-	public static void showList(String url) throws IOException{
-		Document doc = Jsoup.connect(url).get();
+	/*
+	 * @param url
+	 * @Author: Piccus
+	 * @Description: 保存爬取数据到HashMap
+	 */
+	private static void showList(String url) throws IOException{
+		Document doc = Jsoup.connect(url).userAgent(agent).get();
 		Element content = doc.getElementById("content_left");
-		Elements links = content.getElementsByClass("c-title");
+		Elements links = content.getElementsByClass("result");
 		for(Element link : links){
-			Elements linka = link.getElementsByTag("a");
+			Elements linka = link.select("a");
 			String linkHref = linka.attr("href");
 			String linkText = linka.text();
 			hm.put(linkText, linkHref);
 		}	
 	}
 	
+	/*
+	 * @param listMAx
+	 * @Author: Piccus
+	 * @Description: 设置爬取数量
+	 */
+	public static void setListMax(int listMax){
+		BaiduNewsSpider.listMax = listMax;
+	}
 	
+	/*
+	 * @Author: Piccus
+	 * @Desciption: BaiduNewsSpider usage Demo
+	 */
 	public static void main(String args[]) throws IOException{
-		BaiduNewsSpider.showList("http://news.baidu.com/ns?word=%E8%82%A1%E5%B8%82&pn=0&cl=2&ct=0&tn=news&rn=20&ie=utf-8&bt=0&et=0");
-		BaiduNewsSpider.saveList("demo.txt");
+		BaiduNewsSpider.setListMax(20);
+		BaiduNewsSpider.initSpider("股市", "股市.txt");
 	}
 	
 }
