06d65ae07b25fa8f6f7e2e1e1b6be02ee898e315
Piccus <piccus@outlook.com>
2016-03-26 12:12:39 +0800

    simplify baidunewsspider and dbcontrol into tools

--- src/com/piccus/tools/BaiduNewsSpider.java
+++ src/com/piccus/tools/BaiduNewsSpider.java
@@ -12,15 +12,13 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 
-public class BaiduNewsSpider implements Runnable{
+public abstract class BaiduNewsSpider{
 	//HaspMap存储标题和链接
 	private static HashMap<String, String> hm = new HashMap<String, String>();
 	//爬取关键字
 	private static String keyword = "";
 	//实际爬取链接
 	private static String realUrl = "http://news.baidu.com/ns?";
-	//数据存储位置
-	private static String filepath = "";
 	//默认时间顺序爬取
 	private static final int sortByTime = 0;
 	//默认爬取数量	
@@ -36,15 +34,6 @@ public class BaiduNewsSpider implements Runnable{
 	 */
 	public BaiduNewsSpider(String key, String filename){
 		keyword = key;
-		filepath = filename;
-	}
-	
-	/*
-	 * @Author: Piccus
-	 * @Description: 线程开始
-	 */
-	public void run(){
-		initSpider(keyword, filepath);
 	}
 	
 	/*
@@ -53,35 +42,22 @@ public class BaiduNewsSpider implements Runnable{
 	 * @Author: Piccus
 	 * @Description: 初始化爬虫并开始爬取 
 	 */
-	private static void initSpider(String key, String filename){
+	public static void initSpider(String key){
 		keyword = key;
 		realUrl = realUrl + "word=" + keyword + "&tn=news&from=news&rn=" + listMax + "ct=" + sortByTime;
 		try {
-			showList(realUrl);
-			saveList(filename);
+			getHashData(realUrl);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//System.out.println("success get " + listMax + " websites and saving to " + filename);
 	}
 	
 	/*
-	 * @param filename
-	 * @Author: Piccus
-	 * @Description: 保存HashMap到文件
+	 * @Author:Piccus
+	 * @Description:返回爬取数据
 	 */
-	private static void saveList(String filename) throws IOException{
-
-		FileWriter fw = new FileWriter(filename);
-		BufferedWriter bw = new BufferedWriter(fw);
-		for(Entry<String, String> entry : hm.entrySet()){
-			bw.write(entry.getKey());
-			bw.write(entry.getValue());
-			bw.newLine();
-		}
-		bw.flush();
-		fw.close();
-		bw.close();
+	public static HashMap<String, String> getData(){
+		return  hm;
 	}
 	
 	/*
@@ -89,10 +65,10 @@ public class BaiduNewsSpider implements Runnable{
 	 * @Author: Piccus
 	 * @Description: 保存爬取数据到HashMap
 	 */
-	private static void showList(String url) throws IOException{
+	private static void getHashData(String url) throws IOException{
 		Document doc = Jsoup.connect(url).userAgent(agent).get();
 		Element content = doc.getElementById("content_left");
-		Elements links = content.getElementsByClass("result");
+		Elements links = content.getElementsByClass("c-title");
 		for(Element link : links){
 			Elements linka = link.select("a");
 			String linkHref = linka.attr("href");
@@ -109,16 +85,4 @@ public class BaiduNewsSpider implements Runnable{
 	public void setListMax(int listMax){
 		BaiduNewsSpider.listMax = listMax;
 	}
-	
-	/*
-	 * @Author: Piccus
-	 * @Desciption: BaiduNewsSpider thread usage Demo
-	 */
-	public static void main(String args[]) throws IOException{
-		BaiduNewsSpider spider = new BaiduNewsSpider("股票", "demo.txt");
-		spider.setListMax(30);
-		Thread thread = new Thread(spider);
-		thread.start();
-	}
-	
 }
--- src/com/piccus/tools/DBControl.java
+++ src/com/piccus/tools/DBControl.java
@@ -2,25 +2,44 @@ package com.piccus.tools;
 
 import org.sqlite.JDBC;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 public abstract class DBControl {
+	//默认数据库名称
+	private static String dbname = "jdbc:sqlite:baidunews.db";
+	//表存在判断SQL
+	private static String tableExistSql = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='BaiduNews';";
+	//表创建SQL
+	private static String tableCreateSql = "CREATE TABLE BaiduNews(title varchar(50), url varchar(100));" ;
 	
-	public static void main(String[] args) {
+	/*
+	 * @param: HashMap
+	 * @Author: Piccus
+	 * @Description: 存储数据到SQlite 
+	 */
+	public static void Save(HashMap<String, String> hm){
 		try
 		{
 			Class.forName("org.sqlite.JDBC");
 			
-			Connection conn = DriverManager.getConnection("jdbc:sqlite:demo.db");
+			Connection conn = DriverManager.getConnection(dbname);
 			
 			Statement stat = conn.createStatement();
-			stat.executeUpdate("create table users(name varchar(20) primary key);");
 			
-			stat.executeUpdate("insert into users values('piccus');");
+			ResultSet rs = stat.executeQuery(tableExistSql);
+			
+			if(rs.getString(1).equals("0"))
+				stat.executeUpdate(tableCreateSql);
+				
+			for(Entry<String, String> entry : hm.entrySet()){
+				String insertData = "INSERT INTO BaiduNews VALUES('" + entry.getKey() + "', '" + entry.getValue() + "');";
+				stat.executeUpdate(insertData);
+			}
 			conn.close();
-			
-			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
+	
 }
